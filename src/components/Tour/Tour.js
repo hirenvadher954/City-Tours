57cc6fe72a3f02116919835226d7bbb7956c606c
Hiren Vadher <hirenvadher954@gmail.com>
2020-03-27 15:20:38 +0530

    remove and show more button functionality added

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,33 +2,43 @@ import React, {Component} from 'react';
 import './Tour.scss';
 
 class Tour extends Component {
+    state = {
+        showInfo: false
+    };
+
+    handleInfo = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        });
+    };
+
     render() {
+        const {id, city, img, name, info} = this.props.tour;
         return (
             <div>
                 <article className="tour">
                     <div class="img-container">
 
                         <img
-                            src="https://upload.wikimedia.org/wikipedia/en/thumb/6/63/IMG_%28business%29.svg/1200px-IMG_%28business%29.svg.png"
+                            src={img}
                             alt=""
                         />
 
-                        <span className="close-btn">
+                        <span className="close-btn" onClick={() => this.props.removeTour(id)}>
                             <i className="fas fa-window-close"/>
                         </span>
 
                         <div className="tour-info">
-                            <h3>City</h3>
-                            <h4>Name</h4>
+                            <h3>{city}</h3>
+                            <h4>{name}</h4>
                             <h5>Info{" "}
-                                <span>
-                                    <i
-                                        className="fas fa-caret-square-down"
-                                    />
+                                <span onClick={this.handleInfo}>
+                                <i
+                                    className="fas fa-caret-square-down"
+                                />
                                 </span>
                             </h5>
-                            <p>tolosa ipsumBe meaningless for whoever disappears, because each has been met with
-                                advice.</p>
+                            {this.state.showInfo && <p>{info}</p>}
                         </div>
                     </div>
                 </article>
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,17 +1,31 @@
 import React, {Component} from 'react';
 import './TourList.scss';
 import Tour from "../Tour/Tour";
+import {tourData} from './tourData';
 
 class TourList extends Component {
     state = {
-        tours: []
+        tours: tourData
+    };
+
+    handleRemove = (id) => {
+        const {tours} = this.state;
+        this.setState({
+            tours: tours.filter(tours => tours.id !== id)
+        })
     };
 
     render() {
+        const {tours} = this.state;
         return (
             <div>
-                <section className="tourlist">
-                    <Tour/>
+                <section className="tourlist">{
+                    tours.map(tour => <Tour
+                        key={tour.id}
+                        tour={tour}
+                        removeTour={this.handleRemove}/>
+                    )
+                }
                 </section>
             </div>
         );
